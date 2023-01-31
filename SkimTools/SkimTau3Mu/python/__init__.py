60c6f2b4949c665ee5c07cf34aeec162442f6af5
caruta <42809546+caruta@users.noreply.github.com>
2023-01-31 10:45:31 +0100

    Delete SkimTools directory

--- SkimTools/SkimTau3Mu/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/SkimTools/SkimTau3Mu/',1)[0])+'/cfipython/slc7_amd64_gcc10/SkimTools/SkimTau3Mu')
