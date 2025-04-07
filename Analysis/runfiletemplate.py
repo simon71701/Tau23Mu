7f586be1e26dcc8c26cd1ff26cc2b916166c5db0
Benjamin Simon <simon73@purdue.edu>
2025-04-07 20:24:19 +0200

    runfiletemplate.py fix

--- Analysis/runfiletemplate.py
+++ Analysis/runfiletemplate.py
@@ -2,14 +2,13 @@ import subprocess
 
 def get_path(i, era, v):
 
-	i = 1
-	era = 'D'
-	v = '1'
 	file = f'/store/user/bsimon/ParkingDoubleMuonLowMass{i}/SkimPhiPi_2024era{era}_v{v}_stream{i}_Mini_v1'
 
 
 
 	output = subprocess.check_output(['gfal-ls', 'davs://eos.cms.rcac.purdue.edu:9000'+file], text=True)
+	_, output = output.split('\n')
+
 	output = output.replace('\n', '')
 
 	new_file = file + '/' + output
